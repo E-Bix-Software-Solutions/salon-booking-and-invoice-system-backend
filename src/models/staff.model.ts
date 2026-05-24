258f540b4c618d12d9737d589c71318fe63e9817
HansakaV <maheshhansaka628@gmail.com>
2026-05-24 19:15:35 +0530

    fix:fixed runtime errors

--- src/models/staff.model.ts
+++ src/models/staff.model.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import type { StaffFormData } from "../../types/staff.ts";
+import type { StaffFormData } from "../types/staff";
 
 const StaffModel = new mongoose.Schema<StaffFormData>({
     fullName: {
